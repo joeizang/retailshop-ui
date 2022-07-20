14bf2cbdda5d614f6c11b44e06bdfb79a96baf0d
Joseph Izang <josephizang@gmail.com>
2022-07-20 02:35:32 +0100

    add server external contract types

---
+++ src/types/server-types/index.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthenticationResult';
+export * from './CreateProductInputModel';
+export * from './CreateSaleInputModel';
+export * from './DeleteProductModel';
+export * from './GetAllProductsModel';
+export * from './GetAllProductsModelIEnumerablePagedResponse';
+export * from './GetAllProductsModelResponse';
+export * from './GuidResponse';
+export * from './Product';
+export * from './ProductSold';
+export * from './ProductSoldIEnumerableResponse';
+export * from './ProductSoldResponse';
+export * from './ProductToBeSold';
+export * from './Sale';
+export * from './SaleIEnumerableResponse';
+export * from './SaleResponse';
+export * from './ShopUser';
+export * from './UnitMeasure';
+export * from './UpdateProductInputModel';
+export * from './UpdateSaleInputModel';
+export * from './UserDto';
